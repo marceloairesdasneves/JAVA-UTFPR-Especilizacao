ec562a8a3715aa7c26ee92582cb9f836f3443e1c
marceloairesdasneves <141940013+marceloairesdasneves@users.noreply.github.com>
2025-11-13 15:56:25 -0300

    Enviando para o GIT exercicios do Livro de auto estudos

--- OneDrive/Documents/NetBeansProjects/exeLivroJava/src/main/java/br/com/ativinterface/exelivrojava/Help.java
+++ OneDrive/Documents/NetBeansProjects/exeLivroJava/src/main/java/br/com/ativinterface/exelivrojava/Help.java
@@ -4,6 +4,8 @@
  */
 package br.com.ativinterface.exelivrojava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
@@ -13,8 +15,23 @@ public class Help {
         System.out.println("Help on: ");
         System.out.println(" 1. if");
         System.out.println(" 2. switch");
-        System.out.println("Choose one: ");
+        System.out.print("\nChoose one: ");
+        Scanner scanner = new Scanner(System.in);
+        char choice = scanner.next().charAt(0);
         
+        switch(choice){
+            case '1':
+                System.out.println("\nYour choice is `if`");
+                System.out.println("\nif (condition) statement");
+                break;
+            case '2': 
+                System.out.println("Your choice is `switch` "); 
+                System.out.println("switch (expression)");
+                System.out.println(" case constant: ");
+                break;
+            default:
+                System.out.println("Selection not found. ");
+            
+        }
     }
-    
 }
